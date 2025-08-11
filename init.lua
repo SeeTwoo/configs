602bab83af1699f0b266ce7ab2e58a4dea58063e
SeeTwoo <walti.bee@gmail.com>
2025-08-11 07:19:31 +0200

    A

--- init.lua
+++ init.lua
@@ -5,7 +5,9 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.guicursor = ""
+vim.opt.colorcolumn = "81"
+
 vim.cmd("highlight Normal guibg=#000000")
-vim.cmd("highlight Type guifg=#00cdcd")
-vim.cmd("highlight Statement guifg=#ff50ff")
-vim.opt.colorcolumn = "80"
+vim.cmd("highlight ColorColumn ctermbg=lightgrey guibg=lightgrey")
+
+require("fileheader");
