43f922664e2056539d95de87789f8e412ed15c30
SeeTwoo <walti.bee@gmail.com>
2025-08-21 21:49:01 +0200

    new header and init.lua

--- blue_pink_theme.lua
+++ blue_pink_theme.lua
@@ -2,16 +2,18 @@ vim.opt.termguicolors = true
 
 local	function MyHighlighting()
 	vim.api.nvim_set_hl(0, "Statement", {fg = "DeepPink"})
-	vim.api.nvim_set_hl(0, "PreProc", {fg = "LightSkyBlue"})
-	vim.api.nvim_set_hl(0, "Number", {fg = "Plum"})
-	vim.api.nvim_set_hl(0, "Character", {fg = "Purple"})
-	vim.api.nvim_set_hl(0, "Special", {fg = "Violet"})
-	vim.api.nvim_set_hl(0, "String", {fg = "DeepPink3"})
+	vim.api.nvim_set_hl(0, "PreProc", {fg = "LightBlue"})
 	vim.api.nvim_set_hl(0, "Type", {fg = "Cyan"})
 	vim.api.nvim_set_hl(0, "Comment", {fg = "SteelBlue"})
 	vim.api.nvim_set_hl(0, "LineNr", {fg = "SteelBlue"})
-	vim.api.nvim_set_hl(0, "Macro", {fg = "DeepPink3"})
-	vim.api.nvim_set_hl(0, "ColorColumn", {bg = "Grey"})
+
+	vim.api.nvim_set_hl(0, "Character", {fg = "Lavender"})
+	vim.api.nvim_set_hl(0, "Special", {fg = "Plum"})
+	vim.api.nvim_set_hl(0, "String", {fg = "Thistle"})
+	vim.api.nvim_set_hl(0, "Number", {fg = "Violet"})
+	vim.api.nvim_set_hl(0, "Macro", {fg = "Orchid"})
+
+	vim.api.nvim_set_hl(0, "ColorColumn", {bg = "DimGrey"})
 	vim.api.nvim_set_hl(0, "Normal", {bg = "Black"})
 end
 
